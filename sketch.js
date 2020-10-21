d6fc1435aec409b581aa41181bd527eb872863b3
kavyadiju <70419997+kavyadiju@users.noreply.github.com>
2020-10-21 21:26:30 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,44 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+
+var dustbinObj, paperObject,groundObject;	
+var engine,world;
+
+
+function setup() {
+	createCanvas(1600, 700);
+
+	engine = Engine.create();
+	world = engine.world;
+
+	dustbinObj=new dustbin(1200,650);
+	paperObject=new paper(200,450,40);
+	groundObject=new ground(width/2,690,width,20);
+  
+}
+
+
+function draw() {
+
+  background(255,255,255);
+  Engine.update(engine);
+ 
+  paperObject.display();
+  dustbinObj.display();
+  groundObject.display();
+  
+ 
+}
+
+function keyPressed() {
+  	if (keyCode === UP_ARROW) {
+
+    	Matter.Body.applyForce(paperObject.body,paperObject.body.position,{x:85,y:-90});
+    
+  	}
+}
+
+
+
